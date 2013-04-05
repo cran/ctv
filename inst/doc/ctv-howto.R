512e233f906a41b80c1e5ee91a0facffb76b4cd3
Achim Zeileis <Achim.Zeileis@R-project.org>
2013-04-05 00:00:00 +0000

    version 0.7-7

---
+++ inst/doc/ctv-howto.R
@@ -0,0 +1,10 @@
+### R code from vignette source 'ctv-howto.Rnw'
+
+###################################################
+### code chunk number 1: ctv-howto.Rnw:113-116
+###################################################
+library("ctv")
+x <- read.ctv(file.path(.find.package("ctv"), "ctv", "Econometrics.ctv"))
+x
+
+
